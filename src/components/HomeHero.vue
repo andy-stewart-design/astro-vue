cf8d29f5d186055739b6515ea42bbc8aa49eca8c
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2022-05-03 07:12:23 -0700

    Initial commit

---
+++ src/components/HomeHero.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const test: string = "This is a headline";
+
+let isActive = ref<boolean>(false);
+function toggleActive() {
+  isActive.value = !isActive.value;
+}
+</script>
+
+<template>
+  <section class="px-8 py-24 min-h-screen">
+    <h1>{{ test }} {{ isActive }}</h1>
+    <button
+      class="px-4 py-2 text-white bg-blue-600 rounded-full"
+      @click="toggleActive"
+      :class="{ active: isActive }"
+    >
+      Click Me
+    </button>
+  </section>
+</template>
+
+<style>
+.active {
+  @apply bg-black;
+}
+</style>
